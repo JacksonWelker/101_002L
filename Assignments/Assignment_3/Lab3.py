5f2d574889617a22cea4bb14f6895ebc889018c0
JacksonWelker <90873754+JacksonWelker@users.noreply.github.com>
2021-09-29 19:50:58 -0500

    Lab3

---
+++ Assignments/Assignment_3/Lab3.py
@@ -0,0 +1,42 @@
+print('Welcome to Flarsheim Guesser!')
+
+print("Please think of a number between 1 and 100.")
+
+while guess_again == "Y":
+    remainder3 = int(input("What is the remainder when your number is divided by 3?:  "))
+    reminder5 = int(input("What is the remainder when your number is divided by 5?:  "))
+    remainder7: int(input("What is the remainder when your number is divided by 7?:  "))
+    print("Your number was", %remainder3%3)
+    print("How amazing is that?")
+
+guess_again = input("Do you want to play again? Y to condinue, N to quit:  ")
+if guess_again == 'Y':
+    print("Exiting Program")
+
+What is the remainder when your number is divided by 3 ?-1
+The value entered must be 0 or greater What is the remainder when your number is divided by 3 ?3
+The value entered must be less than 3
+What is the remainder when your number is divided by 3 ?5
+The value entered must be less than 3
+What is the remainder when your number is divided by 3 ?1
+
+What is the remainder when your number is divided by 5 ?2
+What is the remainder when yournumber is divided by 7 ?3
+Your number was  52
+How amazing is that?
+
+Do you want to play again? Y to continue, N to quit  ==> e
+Do you want to play again? Y to continue, N to quit  ==> exit
+Do you want to play again? Y to continue, N to quit  ==> y
+
+Pleasethink of a number between and including 1 and 100.
+
+What is the remainder when your number is divided by 3 ?2
+
+What is the remainder when your number is divided by 5 ?1
+
+What is the remainder when your number is divided by 7 ?4
+Your number was  11
+How amazing is that?
+
+Do you want to play again? Y to continue, N to quit  ==> n
